e5b3b1ca8696ff490e89722c5b5bf48f7496a31d
Shihab Shaikh <info@dynamicdreamz.com>
2025-09-15 13:06:05 +0530

    upgrade ui of add brand

---
+++ src/components/Modal.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 bg-opacity-50">
+      <div
+        ref={modalRef}
+        className="relative w-full max-w-fit p-8 bg-white rounded-lg shadow-lg"
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-800"
+        >
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
