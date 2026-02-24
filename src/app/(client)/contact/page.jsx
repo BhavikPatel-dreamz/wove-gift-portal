a74b14d7497c42e39204c99340fc91c2fd2de5d2
Shihab Shaikh <info@dynamicdreamz.com>
2026-02-24 13:07:22 +0530

    change logo

--- src/app/(client)/contact/page.jsx
+++ src/app/(client)/contact/page.jsx
@@ -1,6 +1,3 @@
-"use client"
-import { Provider } from 'react-redux'
-import store from '../../../redux/store'
 import Header from '../../../components/client/home/Header';
 import ContactMain from '../../../components/client/contact/ContactMain';
 import Footer from "../../../components/client/home/Footer"
@@ -8,13 +5,11 @@ import Footer from "../../../components/client/home/Footer"
 const page = () => {
 
     return (
-        <Provider store={store}>
-            <div className="min-h-screen bg-gray-50">
-                <Header />
-                <ContactMain/>
-                <Footer />
-            </div>
-        </Provider>
+        <div className="min-h-screen bg-gray-50">
+            <Header />
+            <ContactMain />
+            <Footer />
+        </div>
     )
 }
 
