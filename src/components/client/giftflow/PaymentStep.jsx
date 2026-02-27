23e2fb3dcd7d7d10958aeb144fe46926a36b4570
Shihab Shaikh <info@dynamicdreamz.com>
2026-02-27 14:44:49 +0530

    update for validation

--- src/components/client/giftflow/PaymentStep.jsx
+++ src/components/client/giftflow/PaymentStep.jsx
@@ -362,7 +362,12 @@ const PaymentStep = () => {
                   </>
                 ) : (
                   <>
-                    Pay with PayFast <span>→</span>
+                    Pay with PayFast   <svg width="8" height="9" viewBox="0 0 8 9" fill="none">
+                      <path
+                        d="M6.75 2.80128C7.75 3.37863 7.75 4.822 6.75 5.39935L2.25 7.99743C1.25 8.57478 0 7.85309 0 6.69839V1.50224C0 0.347537 1.25 -0.374151 2.25 0.2032L6.75 2.80128Z"
+                        fill="white"
+                      />
+                    </svg>
                   </>
                 )}
               </button>
@@ -389,7 +394,12 @@ const PaymentStep = () => {
                   </>
                 ) : (
                   <>
-                    Pay with PayFast <span>→</span>
+                    Pay with PayFast   <svg width="8" height="9" viewBox="0 0 8 9" fill="none">
+                      <path
+                        d="M6.75 2.80128C7.75 3.37863 7.75 4.822 6.75 5.39935L2.25 7.99743C1.25 8.57478 0 7.85309 0 6.69839V1.50224C0 0.347537 1.25 -0.374151 2.25 0.2032L6.75 2.80128Z"
+                        fill="white"
+                      />
+                    </svg>
                   </>
                 )}
               </button>
